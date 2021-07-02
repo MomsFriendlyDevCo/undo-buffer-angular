19e1a739ef36414a577ffe6b0471ec7e195b3e29
Mr-Yellow <mr-yellow@mr-yellow.com>
2021-07-02 10:59:16 +1000

    Initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+//const debug = require('debug')('undo-buffer-angular:main');
+const UndoBuffer = require('undo-buffer');
+
+angular
+	.module('undo-buffer-angular', [])
+	.provider('$undoBuffer', function() {
+
+		const $undoBuffer = function($rootScope) {
+			this._undoBuffer;
+			this.enabled = true;
+
+			this.add = doc => {
+				console.log('add', doc);
+				// New instance tracking each object added.
+				this._undoBuffer = new UndoBuffer({
+					objectHash: function(d, i) {
+						// Allow matching for arrays of objects by object key rather than array index.
+						if (d && Object.prototype.hasOwnProperty.call(d, '_id')) {
+							return d._id;
+						} else if (d && Object.prototype.hasOwnProperty.call(d, 'id')) {
+							return d.id;
+						} else {
+							return '$$index:' + i;
+						}
+					},
+				});
+				$rootScope.$watch(scope => doc, (newVal, oldVal, scope) => {
+					// FIXME: Ignore changes made by undo itself. Otherwise undo = 1 step with redo
+					this._undoBuffer.update(newVal, oldVal);
+				}, true);
+			};
+
+			this.undo = doc => this._undoBuffer.undo(doc);
+			this.redo = doc => this._undoBuffer.redo(doc);
+		};
+
+		/**
+		* Return a new instance for each controller
+		*/
+		return {
+			$get: function($rootScope) {
+				return new $undoBuffer($rootScope);
+			},
+		};
+	});
