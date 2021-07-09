2a65368e5ff3bb90328d6cea50e4ec3fb40c0c0c
Mr-Yellow <mr-yellow@mr-yellow.com>
2021-07-09 15:13:01 +1000

    Returning an instance

--- README.md
+++ README.md
@@ -7,23 +7,23 @@ Undo-Buffer-Angular
 app.factory('angular-factory', function($undoBuffer) {
     const obj = {};
 
-    $undoBuffer.add(_.omit(state, [
+    const undoBuffer = $undoBuffer.watch(_.omit(state, [
         'excluded',
         'fields',
     ]));
 
     obj.doUndo = function() {
-        const orig = $undoBuffer.enabled;
-        $undoBuffer.enabled = false;
-        Object.assign(obj, $undoBuffer.undo(obj));
-        $rootScope.$evalAsync(() => $undoBuffer.enabled = orig);
+        const orig = undoBuffer.enabled;
+        undoBuffer.enabled = false;
+        Object.assign(obj, undoBuffer.undo(obj));
+        $rootScope.$evalAsync(() => undoBuffer.enabled = orig);
     };
 
     obj.doRedo = function() {
-        const orig = $undoBuffer.enabled;
-        $undoBuffer.enabled = false;
-        Object.assign(obj, $undoBuffer.redo(obj));
-        $rootScope.$evalAsync(() => $undoBuffer.enabled = orig);
+        const orig = undoBuffer.enabled;
+        undoBuffer.enabled = false;
+        Object.assign(obj, undoBuffer.redo(obj));
+        $rootScope.$evalAsync(() => undoBuffer.enabled = orig);
     };
 });
 ```
--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-const debug = require('debug')('undo-buffer-angular:main');
 const UndoBuffer = require('undo-buffer');
 
 angular
 	.module('undo-buffer-angular', [])
 	.provider('$undoBuffer', function() {
-
 		/**
 		 * $undoBuffer
 		 *
@@ -12,18 +10,14 @@ angular
 		 * @param {Object} $rootScope Dependancy injection from AngularJS
 		 */
 		const $undoBuffer = function($rootScope) {
-			this._undoBuffer;
-
 			/**
 			 * Deeply watch variable for changes
 			 *
 			 * @type {method}
 			 * @param {Object} doc Object to observe for changes
 			 */
-			this.add = doc => {
-				debug('add', doc);
-				// New instance tracking each object added {{{
-				this._undoBuffer = new UndoBuffer({
+			this.watch = doc => {
+				const undoBuffer = new UndoBuffer({
 					objectHash: function(d, i) {
 						// Allow matching for arrays of objects by object key rather than array index.
 						if (d && Object.prototype.hasOwnProperty.call(d, '_id')) {
@@ -35,45 +29,14 @@ angular
 						}
 					},
 				});
-				// }}}
 
 				$rootScope.$watch(scope => doc, (newVal, oldVal, scope) => {
-					this._undoBuffer.update(newVal, oldVal);
+					undoBuffer.update(newVal, oldVal);
 				}, true);
+
+				return undoBuffer;
 			};
-
-			/**
-			 * Execute an undo action
-			 *
-			 * @type {method}
-			 * @param {Object} doc The current state to patch
-			 */
-			this.undo = doc => this._undoBuffer.undo(doc);
-
-			/**
-			 * Execute a redo action
-			 *
-			 * @type {method}
-			 * @param {Object} doc The current state to patch
-			 */
-			this.redo = doc => this._undoBuffer.redo(doc);
 		};
-
-		/**
-		 * Execute and undo action
-		 * Passes value straight through to underlying instance
-		 *
-		 * @type {property}
-		 * @return {Boolean} Whether or not changes to object will be handled
-		 */
-		Object.defineProperty($undoBuffer.prototype, 'enabled', {
-			get: function() {
-				return this._undoBuffer.enabled;
-			},
-			set: function(val) {
-				this._undoBuffer.enabled = val;
-			},
-		});
 
 		/**
 		* Return a new instance for each controller
